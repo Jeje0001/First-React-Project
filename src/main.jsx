03a57a99b4bbebfa33f39f0b491dcc4447ee0571
jejea <157323284+Jeje0001@users.noreply.github.com>
2025-01-17 18:26:30 -0500

    first project

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,15 @@ import App from './App.jsx'
 const d=createRoot(document.getElementById("root"))
 d.render(
 
-  <div>
-    <img src="" alt="" />
+  <div id="roo">
+    <img src="https://media2.dev.to/dynamic/image/width=1080,height=1080,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2F096baapsqqt9fks0us99.png" alt="" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first realeased in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
   </div>
 )
